6a01b42dc29ca73c139f7a7fb2e4cbb85f1b1459
wy <panzer.wy@gmail.com>
2017-09-11 14:19:00 -0500

    Try update gitattributes

--- MiniMeshFrame/DictMesh.h
+++ MiniMeshFrame/DictMesh.h
@@ -1,7 +1,9 @@
 #pragma once
+
 #include <OpenMesh\Core\Mesh\PolyMesh_ArrayKernelT.hh>
 #include <OpenMesh\Core\Mesh\PolyMeshT.hh>
-#include <OpenMesh\Core\Mesh\Attributes.hh>
+#include <OpenMesh\Core\Mesh\TriMesh_ArrayKernelT.hh>
+#include <OpenMesh\Core\Mesh\Traits.hh>
 
 /*
 This Program shall define the mesh we used in this program.
@@ -9,14 +11,22 @@ This Program shall define the mesh we used in this program.
 
 struct MyTraits : public OpenMesh::DefaultTraits
 {
-	//Customized Traits/Properties
-	VertexAttributes(OpenMesh::Attributes::Normal | OpenMesh::Attributes::Color);
-	FaceAttributes(OpenMesh::Attributes::Normal);
-	
+  // use double valued coordinates
+  typedef OpenMesh::Vec3d Point;
+  // use vertex normals and vertex colors
+  VertexAttributes(OpenMesh::Attributes::Normal | OpenMesh::Attributes::Color);
+  // store the previous halfedge
+  Half
+  HalfedgeAttributes( OpenMesh::Attributes::PrevHalfedge );
+  // use face normals
+  FaceAttributes( OpenMesh::Attributes::Normal );
+  // store a face handle for each vertex
+  VertexTraits
+  {
+    typename Base::Refs::FaceHandle my_face_handle;
+  };
 };
-
-
-typedef OpenMesh::PolyMesh_ArrayKernelT<MyTraits>  MyMesh;
+typedef OpenMesh::TriMesh_ArrayKernelT<MyTraits>  MyMesh;
 
 class DictMesh
 {
